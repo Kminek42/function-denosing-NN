dec0a4c402f98c2cde3047716da7cf201be28da5
kamil <k.kminkowski@icloud.com>
2023-06-11 20:29:07 +0200

    plot titles and description

--- test.py
+++ test.py
@@ -20,6 +20,7 @@ def MSE(x1, x2):
     x2 = x2.detach().numpy()
     return np.mean(np.dot(x1 - x2, x1 - x2))
 
+
 rounds = 10000
 for i in range(rounds):
     target = data_manager.get_clean(samples_n, harmonics)
@@ -29,8 +30,8 @@ for i in range(rounds):
     loss_nn += MSE(target[filter_len:-filter_len], result[filter_len:-filter_len])
     loss_filter += MSE(target[filter_len:-filter_len], conv_res[filter_len:-filter_len])
 
-print(loss_nn / rounds)
-print(loss_filter / rounds)
+print("Neural Network MSE:", loss_nn / rounds)
+print("Low pass filter MSE:", loss_filter / rounds)
 
 while 2137:
     target = data_manager.get_clean(samples_n, harmonics)
@@ -43,9 +44,11 @@ while 2137:
     plt.plot(target, color = "blue", linewidth = 2)
     plt.plot(data_in, color = "red", linewidth = 1)
     plt.plot(result.detach().numpy(), color = "green", linewidth = 4)
+    plt.title("Neural Network denosing")
     plt.show()
 
     plt.plot(target, color = "blue", linewidth = 2)
     plt.plot(data_in, color = "red", linewidth = 1)
     plt.plot(conv_res, color = "green", linewidth = 4)
+    plt.title("Low pass filter denosing")
     plt.show()
