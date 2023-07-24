81adcd560d7b837523c50c3245308120d2d1e84f
Kminek42 <k.kminkowski@icloud.com>
2023-07-24 23:46:38 +0200

    better wave generator

--- data_manager.py
+++ data_manager.py
@@ -4,19 +4,26 @@ import matplotlib.pyplot as plt
 def get_clean(length, max_harmonics):
     harmonics = torch.randint(1, max_harmonics, [1])
     output = torch.tensor([0.0] * length)
-    t = torch.linspace(0, 2 * torch.pi, length)
+    t = torch.linspace(0, 1, length)
     for i in range(harmonics):
-        output += torch.sin(harmonics * torch.rand(1) * t) / (1 + torch.rand(1)) / harmonics
+        output += torch.rand(1) * torch.sin(2 * torch.pi * harmonics * torch.rand(1) * (t + torch.rand(1)))
 
+    output -= torch.min(output)
+    output /= torch.max(output)
+    p1 = 1.8 * torch.rand(1) - 0.9
+    p2 = 1.8 * torch.rand(1) - 0.9
+
+    output *= (p1 - p2)
+    output += p2
     return output
 
 
 def add_noise(samples, max_amplitude):
     return samples + max_amplitude * torch.rand(1) * torch.randn(len(samples))
 
-'''
+
 samples_n = 128
-harmonics = 8
+harmonics = 4
 noise_level = 0.1
 
 while 2137:
@@ -26,4 +33,3 @@ while 2137:
     plt.plot(target, color = "blue", linewidth = 2)
     plt.plot(data_in, color = "red", linewidth = 1)
     plt.show()
-'''
