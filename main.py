583d183687c4a1378d0efa7d64250cc9bc4ef3a3
Kminek42 <k.kminkowski@icloud.com>
2023-07-25 11:25:31 +0200

    code cleanup

--- main.py
+++ main.py
@@ -4,18 +4,25 @@ import matplotlib.pyplot as plt
 import data_manager as dm
 
 # data
-def next_data(channels, dev):
-    target = dm.get_clean(length=128, max_harmonics=16, channels=channels)
+def next_data(*, sample_n, harmonic_n, channel_n, dev):
+    target = dm.get_clean(length=sample_n, max_harmonics=harmonic_n, channels=channel_n)
     data_in = dm.add_noise(samples=target, max_amplitude=0.1)
     return data_in.to(dev), target.to(dev)
 
 
 train = True
 
+# device
+dev = torch.device("cpu")
+
+if torch.cuda.is_available():
+    dev = torch.device("cuda")
+
+if torch.backends.mps.is_available():
+    dev = torch.device("mps")
+
 if train:
     # model
-    dev = torch.device("cuda") if torch.cuda.is_available() else torch.device("mps")
-
     model = nn.Sequential(
         nn.Linear(in_features=128, out_features=128),
         nn.ReLU(),
@@ -30,29 +37,25 @@ if train:
     criterion = nn.MSELoss()
     optimizer = torch.optim.Adam(params=model.parameters(), lr=1e-3)
 
-    epoch_n = 1024
+    epoch_n = 128
 
     loss_sum = 0
 
     for epoch in range(1, epoch_n + 1):
-        data_in, target = next_data(1024, dev)
+        data_in, target = next_data(sample_n=128, harmonic_n=16, channel_n=128, dev=dev)
         prediction = model.forward(data_in)
 
         optimizer.zero_grad()
         loss = criterion(target, prediction)
         loss.backward()
         optimizer.step()
-        loss_sum += float(loss)
-        print(loss_sum)
-        print(epoch)
+        print(float(loss))
 
     torch.save(obj=model, f="model.pt")
 
 
 else:
     # model
-    dev = torch.device("cuda") if torch.cuda.is_available() else torch.device("mps")
-
     model = torch.load(f="model.pt").to(dev)
 
     print(f"Device: {dev}")
